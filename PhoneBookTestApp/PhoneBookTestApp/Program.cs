e250fa3574010bad1e0f4435a7c904ace5feb92e
Jacek Burzynski <jburzyn@gmail.com>
2020-02-28 17:50:21 +0000

    - added PhoneBook implementation and unit tests - implemented 3 of the 4 assignments in Program.cs

--- PhoneBookTestApp/PhoneBookTestApp/Program.cs
+++ PhoneBookTestApp/PhoneBookTestApp/Program.cs
@@ -9,20 +9,42 @@ namespace PhoneBookTestApp
 {
     class Program
     {
-        private PhoneBook _phonebook = new PhoneBook();
+        private static readonly IPhoneBook phoneBook = new PhoneBook();
         
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             try
             {
                 DatabaseUtil.InitializeDatabase();
-                /* TODO: create person objects and put them in the PhoneBook and database
+                
+                /* create person objects and put them in the PhoneBook and database
                 * John Smith, (248) 123-4567, 1234 Sand Hill Dr, Royal Oak, MI
                 * Cynthia Smith, (824) 128-8758, 875 Main St, Ann Arbor, MI
                 */
+                
+                var john = new Person { Name = "John Smith", PhoneNumber = "(248) 123-4567", Address = "1234 Sand Hill Dr, Royal Oak, MI" };
+                var cynthia = new Person { Name = "Cynthia Smith", PhoneNumber = "(824) 128-8758", Address = "875 Main St, Ann Arbor, MI" };
+                
+                phoneBook.AddPerson(john);
+                phoneBook.AddPerson(cynthia);
 
-                // TODO: print the phone book out to System.out
-                // TODO: find Cynthia Smith and print out just her entry
+                // print the phone book out to System.out
+                
+                Console.Write(phoneBook);
+                
+                
+                /* spacing */ Console.WriteLine(); Console.WriteLine();
+                
+                
+                // find Cynthia Smith and print out just her entry
+
+                Person cynthiaFound = phoneBook.FindPerson("Cynthia", "Smith");
+                Console.Write(cynthiaFound);
+                
+                
+                /* spacing */ Console.WriteLine(); Console.WriteLine();
+
+                
                 // TODO: insert the new person objects into the database
 
             }
@@ -30,6 +52,9 @@ namespace PhoneBookTestApp
             {
                 DatabaseUtil.CleanUp();
             }
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadLine();
         }
     }
 }
--- PhoneBookTestApp/PhoneBookTestApp/Tests/PhoneBookTest.cs
+++ PhoneBookTestApp/PhoneBookTestApp/Tests/PhoneBookTest.cs
@@ -1,20 +1,63 @@
-﻿using NUnit.Framework;
+﻿using System;
+using NUnit.Framework;
+using PhoneBookTestApp;
 
 namespace PhoneBookTestAppTests
 {
     [TestFixture]
     public class PhoneBookTest
     {
-        [Test]
-        public void AddPerson()
+        private PhoneBook _phoneBook;
+
+        [SetUp]
+        public void SetUp()
         {
-            Assert.Fail();
+            _phoneBook = new PhoneBook();
         }
 
         [Test]
-        public void FindPerson()
+        public void AddPerson_PersonWithTheSameNameAlreadyExists_ExceptionThrown()
         {
-            Assert.Fail();
+            //arrange
+            const string name = "Derissa Way";
+            
+            var person1 = new Person { Name = name };
+            var person2 = new Person { Name = name };
+            
+            _phoneBook.AddPerson(person1);
+            
+            //act, assert
+            Assert.Throws<ArgumentException>(() => { _phoneBook.AddPerson(person2); });
+        }
+
+        [Test]
+        public void FindPerson_PersonNotFound_NullReturned()
+        {
+            //arrange
+            const string firstName = "Marcus";
+            const string lastName = "Unread";
+            
+            //act
+            Person actual = _phoneBook.FindPerson(firstName, lastName);
+            
+            //assert
+            Assert.IsNull(actual);
+        }
+        
+        [Test]
+        public void AddPersonCombinedWithFindPerson_PersonAdded_PersonFound()
+        {
+            //arrange
+            const string firstName = "Manny";
+            const string lastName = "More";
+            var expected = new Person { Name = firstName + " " + lastName };
+
+            //act
+            _phoneBook.AddPerson(expected);
+            Person actual = _phoneBook.FindPerson(firstName, lastName);
+            
+            //assert
+            Assert.AreSame(expected, actual);
         }
     }
 }
